5d6d3e90a52e2bf4e157b28dd03fff23115bde31
jiikko <n905i.1214@gmail.com>
2022-07-27 20:08:36 +0900

    a

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-export declare const buttons: readonly ["a", "b", "x", "y", "up", "right", "down", "left", "r", "l", "zr", "zl", "plus", "minus", "home", "cap"];
-export declare type Button = typeof buttons[number];
-declare type Props = {
-    pressedButtons: Array<Button>;
-};
-export declare const Procon: ({ pressedButtons }: Props) => JSX.Element;
-export {};
